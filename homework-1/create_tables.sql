63773b296d0dd19dc005ea37937e7edf8a190393
Konstantin Sobolev <comediant683@mail.ru>
2024-03-07 19:20:22 +0300

    home_work1

--- homework-1/create_tables.sql
+++ homework-1/create_tables.sql
@@ -1,1 +1,28 @@
 -- SQL-команды для создания таблиц
+CREATE TABLE employees
+(
+	employee_id int PRIMARY KEY,
+	first_name varchar(100) NOT NULL,
+	last_name varchar(100) NOT NULL,
+	title varchar(100) NOT NULL,
+	birth_date varchar(100) NOT NULL,
+	notes text
+);
+
+CREATE TABLE customer
+(
+	customer_id varchar(100) PRIMARY KEY,
+	company_name varchar(100) NOT NULL,
+	contact_name varchar(100) NOT NULL
+
+);
+
+CREATE TABLE orders
+(
+	order_id int PRIMARY KEY,
+	customer_id varchar(100) REFERENCES customer(customer_id) NOT NULL,
+	contact_name varchar(100) NOT NULL,
+	employee_id int REFERENCES employees(employee_id) NOT NULL,
+	order_date date NOT NULL,
+	ship_city varchar(100) NOT NULL
+)
--- homework-1/main.py
+++ homework-1/main.py
@@ -1,1 +1,35 @@
 """Скрипт для заполнения данными таблиц в БД Postgres."""
+import psycopg2
+import csv
+
+
+def file_reader(path):
+    with open(path) as files:
+        csv_reader = csv.reader(files)
+        count = 0
+        result = []
+        for row in csv_reader:
+            if count == 0:
+                count += 1
+            else:
+                result.append(row)
+                count += 1
+    return result
+
+
+customers_data = file_reader('north_data/customers_data.csv')
+employees_data = file_reader('north_data/employees_data.csv')
+orders_data = file_reader('north_data/orders_data.csv')
+
+with psycopg2.connect(host='localhost', database='north', user='postgres', password='Wanted13') as conn:
+    with conn.cursor() as cur:
+        for customer in customers_data:
+            cur.execute('INSERT INTO customer VALUES(%s, %s, %s)',
+                        (customer[0], customer[1], customer[2]))
+        for employee in employees_data:
+            cur.execute('INSERT INTO employees VALUES(%s, %s, %s, %s, %s, %s)',
+                        (employee[0], employee[1], employee[2], employee[3], employee[4], employee[4]))
+        for orders in orders_data:
+            cur.execute('INSERT INTO employees VALUES(%s, %s, %s, %s, %s)',
+                        (orders[0], orders[1], orders[2], orders[3], orders[3]))
+conn.close()
